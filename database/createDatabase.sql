288e66fec9e36b3bbe0d4ae5019a88978c7426a9
jaybono30@hotmail.com <jaybono30@hotmail.com>
2013-08-16 12:34:00 +0100

    Added folder to hold db creation scripts

---
+++ database/createDatabase.sql
@@ -0,0 +1,13 @@
+--Drop the code example database
+DROP codeExampleDb;
+
+--Create the code example Database
+CREATE DATABASE codeExampleDb;
+
+--Drop code example user
+DROP USER 'codeExample'@'localhost';
+
+--Create code example  user
+CREATE USER 'codeExample'@'localhost' IDENTIFIED BY 'codeExample';
+
+grant all privileges on codeExampleDb.* to codeExample@localhost ;
